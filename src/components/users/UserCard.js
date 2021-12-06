b32d90424387f81b228731aeaf2a53cad4533e5c
martyn <martyn.clews@signavio.com>
2021-12-06 15:01:40 +0100

    updated UserCard

--- src/components/users/UserCard.js
+++ src/components/users/UserCard.js
@@ -3,7 +3,11 @@ import { Link } from "react-router-dom";
 
 const UserCard = ({ user }) => {
     return (
-        <Link to={`/user/${user.id}`} data-testid="userCard">
+        <Link
+            to={`/user/${user.id}`}
+            data-testid="userCard"
+            style={{ textDecoration: "none" }}
+        >
             {user.name}
         </Link>
     );
