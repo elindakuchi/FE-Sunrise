5febbdb4bf32e291a643420c0b9b76b632afcfec
martyn <martyn.clews@signavio.com>
2021-12-07 09:29:20 +0100

    added testing in for user cards

--- src/components/users/UserCard.js
+++ src/components/users/UserCard.js
@@ -24,6 +24,7 @@ const UserCard = ({ user }) => {
                     direction="row"
                     justifyContent="center"
                     alignItems="center"
+                    width="40%"
                 >
                     <CardContent
                         sx={{ flex: "1 0 auto", justifyContent: "center" }}
@@ -34,6 +35,8 @@ const UserCard = ({ user }) => {
                                 height: "102px",
                                 boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
                                 borderRadius: "500px",
+                                marginRight: "20px",
+                                marginLeft: "10px",
                             }}
                             alt="Username"
                         ></Avatar>
@@ -49,18 +52,21 @@ const UserCard = ({ user }) => {
                         sx={{ flex: "1 0 auto", justifyContent: "center" }}
                     >
                         <p
+                            data-testid="user-name"
                             style={{
                                 color: "#AD0F5B",
                                 fontFamily: "Open Sans",
                                 fontStyle: "normal",
                                 fontWeight: 600,
                                 fontSize: "24px",
-                                lineHeight: "32px",
+                                lineHeight: "6px",
+                                //line height on design template at 32px. This creates too much of a gap on app |.| <p>
                             }}
                         >
                             {user.name}
                         </p>
                         <p
+                            data-testid="user-role"
                             style={{
                                 color: "#848484",
                                 fontFamily: "Open Sans",
@@ -74,6 +80,7 @@ const UserCard = ({ user }) => {
                         </p>
 
                         <p
+                            data-testid="user-team"
                             style={{
                                 color: "#050505",
                                 fontFamily: "Open Sans",
@@ -83,7 +90,7 @@ const UserCard = ({ user }) => {
                                 lineHeight: "20px",
                             }}
                         >
-                            {user.location}
+                            @{user.team}
                         </p>
                     </CardContent>
                 </Box>
