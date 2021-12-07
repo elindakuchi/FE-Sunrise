b6f1a7c0e13e86fea1788c512b52dc967ba5103b
Elinda <elinda.kuci@sap.com>
2021-12-07 11:13:35 +0100

    inline styles moved to a separate file

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -6,75 +6,80 @@ import Divider from "@mui/material/Divider";
 import Grid from "@mui/material/Grid";
 import { useParams } from "react-router";
 import { UserContext } from "../../UserProvider";
+import { displayLayout, paperStyles, gridStyles } from "./styles";
 
 const User = () => {
-  const [users] = useContext(UserContext);
-  let { id } = useParams();
-  const USER = users.filter((user) => user.id === id)[0];
-  return (
-    <Paper style={{margin: 40, padding: 20}} elevation={3}>
-          <Grid container spacing={3} >
-                <Grid item md={2.5} xs={12}>
-                    <div
-                        style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        }}
-                    >
-                    <Avatar
-                        alt={USER.name.charAt(0)}
-                        src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80"
-                        sx={{ width: 125, height: 125, marginBottom: 2 }}
-                    >
-                        <Typography variant="h4">{USER.name.charAt(0).toUpperCase()}</Typography>
-                    </Avatar>
-                    <Typography variant="h6" style={{color: '#AD0F5B'}}>{USER.name}</Typography>
-                    <Typography variant="subtitle1" style={{color: '#808080'}}>{USER.role}</Typography>
-                    <Typography variant="subtitle1" >@{USER.team}</Typography>
-                </div>
-                </Grid>
+	const [users] = useContext(UserContext);
+	let { id } = useParams();
+	const USER = users.filter((user) => user.id === id)[0];
+	return (
+		<Paper style={paperStyles} elevation={3}>
+			<Grid container spacing={3}>
+				<Grid item md={2.5} xs={12}>
+					<div style={displayLayout}>
+						<Avatar
+							alt={USER.name.charAt(0)}
+							src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80"
+							sx={{ width: 125, height: 125, marginBottom: 2 }}
+						>
+							<Typography variant="h4">
+								{USER.name.charAt(0).toUpperCase()}
+							</Typography>
+						</Avatar>
+						<Typography variant="h6" style={{ color: "#AD0F5B" }}>
+							{USER.name}
+						</Typography>
+						<Typography variant="subtitle1" style={{ color: "#808080" }}>
+							{USER.role}
+						</Typography>
+						<Typography variant="subtitle1">@{USER.team}</Typography>
+					</div>
+				</Grid>
 
-                <Grid
-                    item
-                    md={0.5}
-                    xs={12}
-                    style={{
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                    }}
-                >
-                    <Divider orientation="vertical" />
-                </Grid>
-                <Grid item md={9} xs={12}>
-                    <Typography align="left" variant="h6">Bio</Typography>
-                    <Divider orientation="horizontal" />
-                    <Typography align="left" gutterBottom style={{color: '#808080'}}>
-                        {USER.bio}
-                    </Typography>
-                <Grid container spacing={2}>
-                    <Grid item md={4} xs={12}>
-                        <Typography align="left" variant="h6">ID</Typography>
-                        <Divider orientation="horizontal" />
-                        <Typography align="left" style={{color: '#808080'}}>{USER.id}</Typography>
-                    </Grid>
-                    <Grid item md={4} xs={12}>
-                        <Typography align="left" variant="h6">Group</Typography>
-                        <Divider orientation="horizontal" />
-                        <Typography align="left" style={{color: '#808080'}}>{USER.group}</Typography>
-                    </Grid>
-                    <Grid item md={4} xs={12}>
-                        <Typography align="left" variant="h6">Team</Typography>
-                        <Divider orientation="horizontal"/>
-                        <Typography align="left" style={{color: '#808080'}}>{USER.team}</Typography>
-                    </Grid>
-                </Grid>
-                </Grid>
-              </Grid>
-      </Paper>
-  );
+				<Grid item md={0.5} xs={12} style={gridStyles}>
+					<Divider orientation="vertical" />
+				</Grid>
+				<Grid item md={9} xs={12}>
+					<Typography align="left" variant="h6">
+						Bio
+					</Typography>
+					<Divider orientation="horizontal" />
+					<Typography align="left" gutterBottom style={{ color: "#808080" }}>
+						{USER.bio}
+					</Typography>
+					<Grid container spacing={2}>
+						<Grid item md={4} xs={12}>
+							<Typography align="left" variant="h6">
+								ID
+							</Typography>
+							<Divider orientation="horizontal" />
+							<Typography align="left" style={{ color: "#808080" }}>
+								{USER.id}
+							</Typography>
+						</Grid>
+						<Grid item md={4} xs={12}>
+							<Typography align="left" variant="h6">
+								Group
+							</Typography>
+							<Divider orientation="horizontal" />
+							<Typography align="left" style={{ color: "#808080" }}>
+								{USER.group}
+							</Typography>
+						</Grid>
+						<Grid item md={4} xs={12}>
+							<Typography align="left" variant="h6">
+								Team
+							</Typography>
+							<Divider orientation="horizontal" />
+							<Typography align="left" style={{ color: "#808080" }}>
+								{USER.team}
+							</Typography>
+						</Grid>
+					</Grid>
+				</Grid>
+			</Grid>
+		</Paper>
+	);
 };
 
 export default User;
---
+++ src/components/users/styles.js
@@ -0,0 +1,17 @@
+export const displayLayout = {
+	display: "flex",
+	flexDirection: "column",
+	justifyContent: "center",
+	alignItems: "center",
+};
+
+export const paperStyles = {
+	margin: 40,
+	padding: 20,
+};
+
+export const gridStyles = {
+	display: "flex",
+	justifyContent: "center",
+	alignItems: "center",
+};
