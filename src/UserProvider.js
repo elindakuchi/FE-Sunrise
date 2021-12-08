0c905e3a75daaee97ac494fcdc71c1189dff9f27
martyn <martyn.clews@signavio.com>
2021-12-08 11:30:58 +0100

    setting up apiCall.js anfd UserProvider.js

--- src/UserProvider.js
+++ src/UserProvider.js
@@ -1,12 +1,20 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import { data } from "./data";
+import { getUsers } from "./apiCall";
 
 export const UserContext = createContext();
 
 const UserProvider = (props) => {
-  const [users, setUsers] = useState(data);
-  const value = [users, setUsers];
-  return <UserContext.Provider value={value} {...props} />;
+    const [users, setUsers] = useState(data);
+    useEffect(() => {
+        async function callingApi() {
+            const data = await getUsers();
+            setUsers(data);
+        }
+        callingApi();
+    }, []);
+    const value = [users, setUsers];
+    return <UserContext.Provider value={value} {...props} />;
 };
 
 export default UserProvider;
---
+++ src/apiCall.js
@@ -0,0 +1,17 @@
+const host = "localhost";
+const port = "9099";
+const url = `http://${host}:${port}/seedlings`;
+
+export const getUsers = async () => {
+    // try {
+    //     const res = await fetch(url);
+    //     const data = await res.json();
+    //     console.log("data in apiCall:", data);
+    // } catch (error) {
+    //     console.log("error in the getUsers function API call:", error);
+    //     return null;
+    // }
+
+    console.log("I am working in apiCall.js");
+    return null;
+};
