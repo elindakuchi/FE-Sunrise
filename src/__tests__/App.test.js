50ece0b596e78f7e5805f634542e0b19c369dd22
NidhiSharmaSignavio <nidhi.sharma@signavio.com>
2021-12-09 15:29:32 +0100

    Pass test display users and display app

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,19 +1,44 @@
-import { render, screen } from "@testing-library/react";
-import { BrowserRouter as Router } from "react-router-dom";
-import App from "../App";
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from '../App';
+import { UserContext } from '../UserProvider';
+import { data as users } from '../data';
 
-const { getByTestId, queryAllByTestId, getByText } = screen;
+const { getByTestId } = screen;
 
-describe("App", () => {
-	it("Should display App", () => {
-		render(
-			<Router>
-				<App />
-			</Router>
-		);
-		expect(getByTestId("app")).toBeInTheDocument();
-		const userList = queryAllByTestId("user-card");
-		expect(userList).toHaveLength(6);
-		expect(getByText("Soria")).toBeInTheDocument();
-	});
+describe('App', () => {
+  it('Should display App', async () => {
+    render(
+      <Router>
+        <App />
+      </Router>
+    );
+    expect(getByTestId('app')).toBeInTheDocument();
+  });
+
+  it('should display list of users when there are users', async () => {
+    render(
+      <UserContext.Provider value={users}>
+        <Router>
+          <App />
+        </Router>
+      </UserContext.Provider>
+    );
+    const userList = await screen.findAllByTestId('user-card');
+    expect(userList.length).toBeGreaterThan(0);
+    await userList;
+  });
+
+  it('should display Soria as a user', async () => {
+    render(
+      <UserContext.Provider value={users}>
+        <Router>
+          <App />
+        </Router>
+      </UserContext.Provider>
+    );
+    const soriaCard = await screen.queryByText(users[0].name);
+    expect(soriaCard.length).toBeGreaterThan(0);
+    await soriaCard;
+  });
 });
